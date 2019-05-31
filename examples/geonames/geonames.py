7ae37312e498b515e38296252318086f7404eb2e
Alex Chamberlain <alex@alexchamberlain.co.uk>
2019-05-31 07:37:45 +0100

    [WiP] More geonames work

--- examples/geonames/geonames.py
+++ examples/geonames/geonames.py
@@ -1,3 +1,4 @@
+import decimal
 import csv
 
 from hexastore.memory import InMemoryHexastore
@@ -5,25 +6,29 @@ from hexastore.ast import IRI
 from hexastore.turtle_serialiser import serialise
 
 CONFIG = [
-    ("geonameid", None),
-    ("name", IRI("http://www.geonames.org/ontology#name")),
-    ("asciiname", None),
-    ("alternatenames", None),  # altenativenames removed by choice
-    ("latitude", IRI("http://www.w3.org/2003/01/geo/wgs84_pos#lat")),
-    ("longitude", IRI("http://www.w3.org/2003/01/geo/wgs84_pos#long")),
-    ("feature class", IRI("http://www.geonames.org/ontology#featureClass")),
-    ("feature code", IRI("http://www.geonames.org/ontology#featureCode")),
-    ("country code", IRI("http://www.geonames.org/ontology#countryCode")),
-    ("cc2", None),
-    ("admin1 code", IRI("http://www.geonames.org/ontology#parentADM1")),
-    ("admin2 code", IRI("http://www.geonames.org/ontology#parentADM2")),
-    ("admin3 code", IRI("http://www.geonames.org/ontology#parentADM3")),
-    ("admin4 code", IRI("http://www.geonames.org/ontology#parentADM4")),
-    ("population", IRI("http://www.geonames.org/ontology#population")),
-    ("elevation", None),  #  TODO: Not sure what this should be mapped to
-    ("dem", None),
-    ("timezone", None),  #  TODO: Not sure what this should be mapped to
-    ("modification date", None),
+    ("geonameid", None, None),
+    ("name", IRI("http://www.geonames.org/ontology#name"), None),
+    ("asciiname", None, None),
+    ("alternatenames", None, None),  # altenativenames removed by choice
+    ("latitude", IRI("http://www.w3.org/2003/01/geo/wgs84_pos#lat"), decimal.Decimal),
+    ("longitude", IRI("http://www.w3.org/2003/01/geo/wgs84_pos#long"), decimal.Decimal),
+    (
+        "feature class",
+        IRI("http://www.geonames.org/ontology#featureClass"),
+        lambda x: IRI(f"http://www.geonames.org/ontology#{x}"),
+    ),
+    ("feature code", IRI("http://www.geonames.org/ontology#featureCode"), None),
+    ("country code", IRI("http://www.geonames.org/ontology#countryCode"), None),
+    ("cc2", None, None),
+    ("admin1 code", IRI("http://www.geonames.org/ontology#parentADM1"), None),
+    ("admin2 code", IRI("http://www.geonames.org/ontology#parentADM2"), None),
+    ("admin3 code", IRI("http://www.geonames.org/ontology#parentADM3"), None),
+    ("admin4 code", IRI("http://www.geonames.org/ontology#parentADM4"), None),
+    ("population", IRI("http://www.geonames.org/ontology#population"), int),
+    ("elevation", None, None),  #  TODO: Not sure what this should be mapped to
+    ("dem", None, None),
+    ("timezone", None, None),  #  TODO: Not sure what this should be mapped to
+    ("modification date", None, None),
 ]
 
 store = InMemoryHexastore()
@@ -44,11 +49,11 @@ xmlns:wgs84_pos="http://www.w3.org/2003/01/geo/wgs84_pos#">
 """
 
 TYPE = IRI("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
-FEATURE = IRI("http://www.geonames.org/ontology#feature")
+FEATURE = IRI("http://www.geonames.org/ontology#Feature")
 
 # with open("/Users/alex/Downloads/allCountries.txt", "r") as fo:
-# with open("london.txt", "r") as fo:
-with open("/Users/alex/Downloads/GB/GB.txt", "r") as fo:
+with open("london.txt", "r") as fo:
+    # with open("/Users/alex/Downloads/GB/GB.txt", "r") as fo:
     reader = csv.reader(fo, delimiter="\t")
     for row in reader:
         iri = IRI(f"http://sws.geonames.org/{row[0]}/")
@@ -56,7 +61,9 @@ with open("/Users/alex/Downloads/GB/GB.txt", "r") as fo:
         store.insert(iri, TYPE, FEATURE, 0)
 
         for config, cell in zip(CONFIG, row):
-            if config[1] is not None:
+            if config[1] is not None and cell:
+                if config[2]:
+                    cell = config[2](cell)
                 store.insert(iri, config[1], cell, 0)
 
 print(f"len(store) = {len(store)}")
